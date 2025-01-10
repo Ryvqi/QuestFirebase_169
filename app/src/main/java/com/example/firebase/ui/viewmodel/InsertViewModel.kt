d8d8418207a983c9d329b4358a8fa7ba27a7c53d
Ryvqi <rifqikhabibi03@gmail.com>
2025-01-10 23:51:50 +0700

    menambahkan data class FormErrorState

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -11,3 +11,17 @@ data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isEntryValid: FormErrorState = FormErrorState()
 )
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jeniskelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+){
+    fun isValid(): Boolean{
+        return nim == null && nama == null && jeniskelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
